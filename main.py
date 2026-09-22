251ab262021d198829180a28d8a45d84b26db58b
rakshitjo007-stack <rakshitjo007@gmail.com>
2026-09-22 22:37:36 +0530

    Build user registration and login

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Welcome to Hostel Issue Analytics System")
+users = {}
+def register():
+    name = input("Enter your name: ")
+    username = input("Create a username: ")
+    if username in users:
+        print("Username already exists!")
+    else:
+        users[username] = name
+        print("Registration successful!")
+        print("Welcome,", name)
+        print("Username:", username)
+while True:
+    print("===== HOSTEL ISSUE PATTERN & ANALYTICS SYSTEM =====")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        register()
+    elif choice == "2":
+        username = input("Enter your username: ")
+        if username in users:
+            print("Login successful!")
+            print("Welcome,", users[username])
+        else:
+            print("Username not found")
+    elif choice == "3":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice")
